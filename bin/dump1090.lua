2ceb1f2eeb7d3bab3d592294459a73755c1f7330
John Kha <sirkha@users.noreply.github.com>
2015-06-17 21:27:29 -0500

    Beginning of a layout with dummy figures

--- bin/dump1090.lua
+++ bin/dump1090.lua
@@ -1,3 +1,30 @@
 #!/usr/bin/env lua
 
-ui = require "tek.ui"
+local ui = require "tek.ui"
+local Group = ui.Group
+local Text = ui.Text
+
+local window = ui.Window:new {
+  Orientation = "vertical",
+  Title = "dash1090",
+  Children = {
+    Group:new {
+      Legend = "Aircraft",
+      Oreintation = "Vertical",
+      Children = {
+        Group:new {
+          Legend="Current",
+          Children = {
+            Text:new { Text = "6" }
+          }
+        }
+      }
+    }
+  }
+}
+
+local app = ui.Application:new()
+ui.Application.connect(window)
+app:addMember(window)
+
+app:run()
