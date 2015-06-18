91b4f69f1b6a6a1f013bcfcd0c360ce705937294
John Kha <sirkha@users.noreply.github.com>
2015-06-18 04:34:46 -0500

    Layout of aircraft includes all desired fields

--- bin/dump1090.lua
+++ bin/dump1090.lua
@@ -4,18 +4,33 @@ local ui = require "tek.ui"
 local Group = ui.Group
 local Text = ui.Text
 
+local txtAircraftCurrent = Text:new { Text = "##" }
+local txtAircraft24hr = Text:new { Text = "##" }
+local txtAircraft7d = Text:new { Text = "##" }
+local txtAircraftMo = Text:new { Text = "##" }
+local txtAircraftAll = Text:new { Text = "##" }
+
 local window = ui.Window:new {
   Orientation = "vertical",
   Title = "dash1090",
   Children = {
     Group:new {
       Legend = "Aircraft",
-      Oreintation = "Vertical",
       Children = {
         Group:new {
           Legend="Current",
           Children = {
-            Text:new { Text = "6" }
+            txtAircraftCurrent
+          }
+        },
+        Group:new {
+          Legend = "Historical",
+          Columns = 2,
+          Children = {
+            Text:new { Class="Caption", Text="24 hr"},    txtAircraft24hr,
+            Text:new { Class="Caption", Text="7 day"},    txtAircraft7d,
+            Text:new { Class="Caption", Text="1 Mo"},     txtAircraftMo,
+            Text:new { Class="Caption", Text="All Time"}, txtAircraftAll
           }
         }
       }
